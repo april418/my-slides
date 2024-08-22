0b285521ea74f270c0d2bc12c42c073c7441a82a
Reo Yoshimoto <reo.yoshimoto@intimatemerger.com>
2024-08-22 00:06:49 +0000

    TOPページにヘッダーを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import {Icon} from '@/features/nes-css/components/Icon';
 import {SlideList} from './_components/SlideList';
 
 export default function Home() {
   return (
-    <main>
-      <SlideList />
-    </main>
+    <>
+      <header>
+        <h1 style={{marginBottom: 0}}>{process.env.NEXT_PUBLIC_GITHUB_REPO}</h1>
+        <a
+          href={`https://github.com/${process.env.NEXT_PUBLIC_GITHUB_OWNER}/${process.env.NEXT_PUBLIC_GITHUB_REPO}`}
+        >
+          <Icon name="github" />
+        </a>
+      </header>
+      <main>
+        <SlideList />
+      </main>
+    </>
   );
 }
